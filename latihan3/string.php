cf0428570a8de683a9b9672c86090febba918a29
AidityasAdhakim <aidityasadhakim250@gmail.com>
2022-04-13 11:15:51 +0800

    Pembelajaran String Method PHP

---
+++ latihan3/string.php
@@ -0,0 +1,14 @@
+<?php 
+
+echo strlen("Aidityas Adhakim");
+echo "<br>";
+echo str_word_count("Aidityas Adhakim");
+echo "<br>";
+echo strrev("Aidityas Adhakim");
+echo "<br>"; 
+echo strpos("Aidityas Adhakim", "Adhakim");
+echo "<br>";
+echo str_replace("Adhakim", "Ganteng","Aidityas Adhakim");
+echo "<br>";
+
+?>
