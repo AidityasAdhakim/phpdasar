8a20bd02f2173576e6462ec3fe7f984f75a0ecc2
AidityasAdhakim <aidityasadhakim250@gmail.com>
2022-04-13 10:48:10 +0800

    Pembelajaran Array

--- latihan3/array.php
+++ latihan3/array.php
@@ -1,10 +1,38 @@
 <?php 
 
-$mobil = array("BMW","Volvo","Mazda");
+// $mobil = array("BMW","Volvo","Mazda");
 
-for($i=0;$i<3;$i++){
-    echo $mobil[$i];
-    echo "<br>";
+// for($i=0;$i<3;$i++){
+//     echo $mobil[$i];
+//     echo "<br>";
+// }
+
+// echo count($mobil);
+
+// Associative Array
+
+// $mobil = array("Mahal"=>"BMW", "Murah" => "Brio", "Sedang"=>"Avanza");
+// echo $mobil["Mahal"];
+// echo $mobil["Murah"];
+// echo $mobil["Sedang"];
+
+// Loop through Associative Array
+// foreach($mobil as $i => $i_value){
+//     echo $i_value;
+//     echo "<br>";
+// }
+
+$mobil = array(
+    array("Avanza","Mobil Sedang","Banyak Dipakai"),
+    array("Brio","Murah","Banyak Dipakai")
+);
+
+for($i=0;$i<count($mobil);$i++){
+    echo "<ul>";
+    for($j=0;$j<3;$j++){
+        echo "<li>Penjelasan Mobi :<br> " . $mobil[$i][$j] . "</li>";
+    }
+    echo "</ul>";
 }
 
  ?>
