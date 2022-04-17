1aa760f6e8fd6e8d3a2d0ae40e1f7fc6227b60d3
AidityasAdhakim <aidityasadhakim250@gmail.com>
2022-04-17 15:02:15 +0800

    Insert & Delete

---
+++ latihan5/functions.php
@@ -0,0 +1,29 @@
+<?php 
+$db = mysqli_connect("localhost", "root", "", "phpdasar");
+    
+    function query($query){
+        global $db;
+        $result = mysqli_query($db, $query);
+        $rows = [];
+        while( $row = mysqli_fetch_assoc($result) ){
+            $rows[] = $row;
+        }
+        return $rows;
+    }
+?>
+
+<?php
+
+function tambah($data){
+    global $db;
+    $nama = $data["nama"];
+    $nim = $data["nim"];
+    $jurusan = $data["jurusan"];
+
+        // query insert data
+        mysqli_query($db, "INSERT INTO mahasiswa VALUES ('','$nama','$nim','$jurusan')");
+    return mysqli_affected_rows($db);
+}
+
+
+?>
---
+++ latihan5/index.php
@@ -0,0 +1,55 @@
+<?php 
+require 'functions.php';
+$mhs = query("SELECT * FROM mahasiswa");
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Database</title>
+</head>
+<body>
+    <h1>Daftar Mahasiswa</h1>
+
+    <a href="tambah.php">Tambah data Mahasiswa</a>
+    <br>
+    <br>
+
+    <table border="3" cellpadding="10" cellspacing="0" >
+       
+    <tr>
+        <th>No.</th>
+        <th>Aksi</th>
+        <th>NIM</th>
+        <th>Nama</th>
+        <th>Jurusan</th>
+    </tr>
+    <?php $i=1; ?>
+    
+    <?php foreach ($mhs as $row) : ?>
+        
+    <tr>
+        <td><?php echo $i; $i++; ?>
+        </td>
+        <td>
+            <a href="">Ubah</a> |
+            <a href="">Hapus</a>
+        </td>
+        <td><?php echo $row["nim"]; ?>
+        </td>
+        <td><?php echo $row["nama"]; ?>
+        </td>
+        <td><?php echo $row["jurusan"]; ?>
+        </td>
+    
+    </tr>
+    <?php endforeach; ?>
+    
+
+    </table>
+</body>
+</html>
