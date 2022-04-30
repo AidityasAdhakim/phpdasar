b90664fce08000cc7a709099446b6d8c7b2b29b0
AidityasAdhakim <aidityasadhakim250@gmail.com>
2022-04-30 11:44:01 +0800

    Pembelajaran Session

---
+++ latihan11/index.php
@@ -0,0 +1,96 @@
+<?php 
+session_start();
+
+if (!isset($_SESSION["login"])){
+    header("Location: login.php");
+    exit;
+}
+
+require 'functions.php';
+$mhs = query("SELECT * FROM mahasiswa");
+
+// Ketika tombol cari ditekan
+if( isset($_POST["search"]) ){
+    $mhs = cari($_POST["keyword"]);
+}
+if(isset($_POST['home'])){
+    $mhs = query("SELECT * FROM mahasiswa");
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Upload</title>
+</head>
+<body>
+    <div class="container">
+    <h1>Daftar Mahasiswa</h1>
+
+    <a href="tambah.php">Tambah data Mahasiswa</a>
+    <br>
+    <br>
+    <form action="" method="post">
+        <button type="submit" name="home">Home</button>
+        <br> <br>
+    </form>
+
+    <form action="" method="post">
+        <label for="search">Cari : </label>
+        <input type="text" name="keyword" size="40" placeholder="Masukkan Nama.." autocomplete="off">
+        <button type="submit" name="search">Search</button>
+        <br>
+        <br>
+        <?php if(isset($_POST['search'])){
+            echo "Hasil Pencarian Keyword " . $_POST["keyword"];
+        } ?>
+    </form>
+
+    <div class="logout">
+        <a href="logout.php">
+        <button type="submit" name="logout">Logout</button>
+        </a>
+    </div>
+
+    <table border="3" cellpadding="10" cellspacing="0" >
+       
+    <tr>
+        <th>No.</th>
+        <th>Aksi</th>
+        <th>NIM</th>
+        <th>Nama</th>
+        <th>Jurusan</th>
+    </tr>
+    <?php $i=1; ?>
+    
+    <?php foreach ($mhs as $row) : ?>
+        
+    <tr>
+        <td><?php echo $i; $i++; ?>
+        </td>
+        <td>
+            <a href="ubah.php?id=<?= $row["id"];?>">Ubah</a> |
+            <a href="hapus.php?id=<?= $row["id"]; ?>" onclick="return confirm('Apakah yakin ingin menghapus?')">Hapus</a>
+        </td>
+        <td><?php echo $row["nim"]; ?>
+        </td>
+        <td><?php echo $row["nama"]; ?>
+        </td>
+        <td><?php echo $row["jurusan"]; ?>
+        </td>
+        <td><img src="img/<?= $row["gambar"]; ?>" style="max-width: 100px; height:auto;">
+        </td>
+    
+    </tr>
+    <?php endforeach; ?>
+    
+
+    </table>
+    </div>
+</body>
+</html>
---
+++ latihan11/login.php
@@ -0,0 +1,72 @@
+<?php 
+session_start();
+
+if(isset($_SESSION["login"])){
+    header("Location: index.php");
+    exit;
+}
+
+require 'functions.php';
+if( isset($_POST["login"]) ){
+
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+
+    $usernameResult = mysqli_query($db, "SELECT * FROM users WHERE username = '$username'");
+
+    if(mysqli_num_rows($usernameResult) === 1 ){
+        
+        // Cek password
+        $row = mysqli_fetch_assoc($usernameResult);
+        if(password_verify($password, $row["password"])){
+            $_SESSION["login"] = true;
+            header("Location: index.php");
+            exit;
+        } else {
+            echo "<script>
+            alert('Password Salah')
+        </script>";
+        }
+
+    } else {
+        echo "<script>
+            alert('Username Salah')
+        </script>";
+    }
+
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Halaman Login</title>
+</head>
+<body>
+    
+    <h1>Halaman Login</h1>
+
+    <form action="" method="POST">
+
+        <ul>
+            <li>
+                <label for="username">Username</label>
+                <input type="text" name="username" id="username">
+            </li>
+            <li>
+                <label for="password">Password</label>
+                <input type="password" name="password" id="password">
+            </li>
+            <br>
+            <button type="submit" name="login">Login</button>
+        </ul>
+
+    </form>
+
+</body>
+</html>
