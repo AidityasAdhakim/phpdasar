c8bf0cf2d5d53d8f659ca5763febaa8d51ecc724
AidityasAdhakim <aidityasadhakim250@gmail.com>
2022-04-30 10:52:02 +0800

    Pembelajaran registrasi

---
+++ latihan9/tambah.php
@@ -0,0 +1,64 @@
+<?php
+require 'functions.php';
+// koneksi ke DBMS
+$db = mysqli_connect("localhost","root","","phpdasar");
+
+if(isset($_POST["submit"])){
+
+//    cek apakah data berhasil ditambah
+    if( tambah($_POST) > 0){
+        echo "
+            <script>
+                alert('Data Berhasil Ditambahkan!')
+                document.location.href = 'index.php'
+            </script>
+        ";
+    } else{
+        echo "
+            <script>
+                alert('Data Gagal Ditambahkan')
+                document.location.href = 'index.php'
+            </script>
+        ";
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tambah</title>
+</head>
+<body>
+    <h1>Tambah Data Mahasiswa</h1>
+
+    <form action="" method="post" enctype="multipart/form-data">
+        <ul>
+            <li>
+                <label for="nama">Nama : </label>
+                <input type="text" name="nama" id="nama" required autocomplete="off">
+            </li>
+
+            <li>
+                <label for="nim">NIM : </label>
+                <input type="text" name="nim" id="nim" required autocomplete="off">
+            </li>
+            <li>
+                <label for="jurusan">Jurusan : </label>
+                <input type="text" name="jurusan" id="jurusan" required autocomplete="off">
+            </li>
+            <li>
+                <label for="gambar">Gambar : </label>
+                <input type="file" name="gambar" >
+            </li>
+            <li>
+                <button type="submit" name="submit">Tambahkan Data</button>
+            </li>
+        </ul>
+
+    </form>
+</body>
+</html>
---
+++ sololearn/index.php
@@ -0,0 +1,23 @@
+<?php 
+echo "Selamat datang " . $_GET["nama"];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>sololearn</title>
+</head>
+<body>
+    <form action="" method="get">
+        <label for="nama" id="nama">Nama : </label>
+        <input type="text" name="nama" id="nama">
+        <button type="submit" name="submit">Submit</button>
+    </form>
+    
+        
+    
+</body>
+</html>
